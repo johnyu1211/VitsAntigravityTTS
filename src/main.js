9b38e6f1454ea440b70f59ddc898fad9ecbd4eb3
johnyu1211 <47817667+johnyu1211@users.noreply.github.com>
2026-08-15 12:33:33 +0900

    fix: Disable Chromium backgroundThrottling and use real timestamp delta to ensure timers never freeze in background

--- src/main.js
+++ src/main.js
@@ -111,7 +111,8 @@ function createWindow() {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
-      contextIsolation: true
+      contextIsolation: true,
+      backgroundThrottling: false
     }
   });
 
